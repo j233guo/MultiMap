ac48e7e0ac19a092f1d15fff325a1fde454ecedd
Jiaming Guo <64711081+j233guo@users.noreply.github.com>
2023-08-03 21:57:29 -0400

    Initial Commit

---
+++ MultiMap/MultiMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiMapApp.swift
+//  MultiMap
+//
+//  Created by Jiaming Guo on 2023-08-03.
+//
+
+import SwiftUI
+
+@main
+struct MultiMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
